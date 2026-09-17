0b9023b92f1b2fdb94e3f9f23d5d15d9c982aebd
sakthivelM <sakthivelm098@gmail.com>
2026-09-17 11:17:01 +0530

    first commit

---
+++ students/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from students.models import Student
+from students.serializers import StudentSerializer
+
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
